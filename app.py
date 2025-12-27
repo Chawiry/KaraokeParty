0d0ba67dd344fc8f4d021fdc749369046842c932
Chawiry <jedelillo@gmail.com>
2025-12-26 23:01:39 -0400

    added song_name check before deleting to advance queue

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import sqlite3
 from flask import Flask, render_template, request, redirect
 
-#For qr code
+# For qr code
 import qrcode
 import base64
 import io
@@ -10,45 +10,47 @@ import io
 app = Flask(__name__)
 
 
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run()
+
 
 @app.route("/")
 def index():
     # Landing page
-    
-    full_url = request.url_root.rstrip('/') + app.url_for('queue')
+
+    full_url = request.url_root.rstrip("/") + app.url_for("queue")
     qr = qrcode.make(full_url)
     img_buffer = io.BytesIO()
-    qr.save(img_buffer, format="PNG")
+    qr.save(img_buffer)
     img_str = base64.b64encode(img_buffer.getvalue()).decode()
 
     return render_template("index.html", qr_data=img_str)
+
 
 @app.route("/queue")
 def queue():
     # Add songs to queue page
     return render_template("queue.html")
 
+
 @app.route("/add_to_queue", methods=["POST"])
 def add_to_queue():
-    # Handles POST requests to add song to queue db 
+    # Handles POST requests to add song to queue db
     song_name = request.form.get("song_name")
     song_url = request.form.get("song_url")
 
     if song_name and song_url:
         if add_song_to_db(song_name, song_url):
-            return render_template('succes.html')
-        return render_template('error.html')
-    return render_template('error.html')
+            return render_template("succes.html")
+        return render_template("error.html")
+    return render_template("error.html")
 
 
 def add_song_to_db(song_name, song_url):
- with sqlite3.connect('song_queue.db') as conn:
+    with sqlite3.connect("song_queue.db") as conn:
         cursor = conn.cursor()
         conn.execute("BEGIN")
-        
+
         # creates a table if it doesnt exist
         conn.execute("""
             CREATE TABLE IF NOT EXISTS song_queue(
@@ -57,12 +59,15 @@ def add_song_to_db(song_name, song_url):
             song_url STRING NOT NULL
             )
         """)
-        
+
         try:
             cursor.execute("SELECT * FROM song_queue")
             rows = cursor.fetchall()
             queue_pos = len(rows)
-            cursor.execute("INSERT INTO song_queue(song_name, queue_pos, song_url) VALUES(?, ?, ?)", (song_name, queue_pos, song_url))
+            cursor.execute(
+                "INSERT INTO song_queue(song_name, queue_pos, song_url) VALUES(?, ?, ?)",
+                (song_name, queue_pos, song_url),
+            )
 
             conn.commit()
             return True
@@ -78,8 +83,7 @@ def visualizer():
         row = cursor.execute("SELECT * FROM song_queue WHERE queue_pos=0;").fetchone()
         if row:
             id = parse_id(row[2])  # index 2 of the tuple should be song url
-            name = row[0] # index 0 should be song name
-            remove_played_song()
+            name = row[0]  # index 0 should be song name
             if id:
                 return render_template("visualizer.html", song_id=id, song_name=name)
             else:
@@ -88,24 +92,34 @@ def visualizer():
 
 
 def parse_id(url):
-    if '(' in url and ')' in url:
-        url = url.split('(')[-1].split(')')[0]
+    if "(" in url and ")" in url:
+        url = url.split("(")[-1].split(")")[0]
 
-    if 'youtu.be/' in url:
-        return url.split('youtu.be/')[1].split('?')[0]
+    if "youtu.be/" in url:
+        return url.split("youtu.be/")[1].split("?")[0]
 
-    if 'v=' in url:
-        return url.split('v=')[1].split('&')[0]
+    if "v=" in url:
+        return url.split("v=")[1].split("&")[0]
 
-    if 'embed/' in url:
-        return url.split('embed/')[1].split('?')[0]
+    if "embed/" in url:
+        return url.split("embed/")[1].split("?")[0]
 
     return None
 
-def remove_played_song():
+
+@app.route("/advance_queue", methods=["POST"])
+def advance_queue():
+    data = request.get_json()
+    song_name = data.get("song_name")
+    print(song_name)
+
     with sqlite3.connect("song_queue.db") as conn:
         cursor = conn.cursor()
-        cursor.execute("DELETE FROM song_queue WHERE queue_pos=0;")
-        cursor.execute("UPDATE song_queue SET queue_pos = queue_pos - 1 WHERE queue_pos > 0;")
+        cursor.execute(
+            "DELETE FROM song_queue WHERE queue_pos=0 AND song_name= ?", (song_name,)
+        )
+        cursor.execute(
+            "UPDATE song_queue SET queue_pos = queue_pos - 1 WHERE queue_pos > 0;"
+        )
         conn.commit()
-    return "Deleted"
+    return "", 200
