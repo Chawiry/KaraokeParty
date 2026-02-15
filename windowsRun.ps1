d8e84d048efded13f7ad186c5f2109f8dea49e94
Jose E Garcia <87092598+Chawiry@users.noreply.github.com>
2026-02-14 21:45:00 -0400

    Update windowsRun.ps1

--- windowsRun.ps1
+++ windowsRun.ps1
@@ -1,7 +1,22 @@
 #!/usr/bin/env pwsh
 
-git clone https://github.com/Chawiry/KaraokeParty.git
-cd ./KaraokeParty
+$repo_url = "https://github.com/Chawiry/KaraokeParty.git"
+$repo_location = "$HOME/Downloads/KaraokeParty"
+
+
+if (Test-Path $repo_location) {
+    Set-Location $repo_location
+    git pull
+} else {
+    # 3. Clone if it doesn't exist
+    git clone $repo_url $repo_location
+    Set-Location $repo_location
+}
+
+if (-not (Test-Path ".\.KPvenv")) {
+    python -m venv .KPvenv
+}
+
 python -m venv .KPvenv
 .\.KPvenv\Scripts\Activate.ps1
 pip install -r requirements.txt
