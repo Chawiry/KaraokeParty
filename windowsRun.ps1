81aa672b36b5a560ac064f50f223707e9ab82a1d
Jose E Garcia <87092598+Chawiry@users.noreply.github.com>
2026-02-14 20:42:49 -0400

    Update windowsRun.ps1

--- windowsRun.ps1
+++ windowsRun.ps1
@@ -1,8 +1,11 @@
 #!/usr/bin/env pwsh
 
+git clone https://github.com/Chawiry/KaraokeParty.git
+cd ./KaraokeParty
 python -m venv .KPvenv
 .\.KPvenv\Scripts\Activate.ps1
 pip install -r requirements.txt
 Start-Process "http://$(hostname):5000"
 flask run --host=$(hostname)
-
+cd ..
+rm ./KaraokeParty
